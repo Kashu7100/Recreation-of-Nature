2daacc9970413715297be9d75538769358bf7cf2
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-02-07 14:10:51 -0600

    Update gray_scott.py

--- patterns/gray_scott.py
+++ patterns/gray_scott.py
@@ -1,1 +1,44 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from moviepy.editor import VideoClip
+from moviepy.video.io.bindings import mplfig_to_npimage
+
+class GrayScott(object):
+    def __init__(self, size=(256,256), Du=2e-5, Dv=1e-5, f=0.035, k=0.065):
+        self.size = size
+        self.Du = Du
+        self.Dv = Dv
+        self.f = f
+        self.k = k
+        self.u = np.ones(size) + np.random.rand(*size)/20
+        self.v = np.zeros(size) + np.random.rand(*size)/20
+        square = 20
+        self.u[size[0]//2-square//2:size[0]//2+square//2,size[1]//2-square//2:size[1]//2+square//2] = 0.5
+        self.v[size[0]//2-square//2:size[0]//2+square//2,size[1]//2-square//2:size[1]//2+square//2] = 0.25
+        
+        self.fig, self.ax = plt.subplots()
+
+    def update(self):
+        lu = np.zeros_like(self.u)
+        lu[1:self.size[0]-1,1:self.size[1]-1] = (np.roll(self.u,1,axis=0)+np.roll(self.u,-1,axis=0)+np.roll(self.u,1,axis=1)+np.roll(self.u,-1,axis=1)-4*self.u)[1:self.size[0]-1,1:self.size[1]-1]/1e-4
+        lv = np.zeros_like(self.v)
+        lv[1:self.size[0]-1,1:self.size[1]-1] = (np.roll(self.v,1,axis=0)+np.roll(self.v,-1,axis=0)+np.roll(self.v,1,axis=1)+np.roll(self.v,-1,axis=1)-4*self.v)[1:self.size[0]-1,1:self.size[1]-1]/1e-4
+        self.u += self.Du*lu - self.u*self.v**2 + self.f*(1-self.u)
+        self.v += self.Dv*lv + self.u*self.v**2 - (self.f+self.k)*self.v
+
+    def visualize(self, t):
+        if t % 10 == 0:
+            self.ax.clear()
+            plt.title('Gray-Scott Model')
+            self.ax.imshow(self.u, interpolation='nearest', cmap="nipy_spectral")
+            plt.axis('off')
+            plt.draw()
+            plt.pause(.001)
+
+    def animation(self, t):
+        self.update()
+        self.ax.clear()
+        plt.title('Gray-Scott Model')
+        self.ax.imshow(self.u, interpolation='nearest', cmap="nipy_spectral")
+        plt.axis('off')
+        return mplfig_to_npimage(self.fig)
